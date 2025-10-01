181d08c64573c8c1eb89721d4bb3f7d28b2f28b2
parkcheayeon <ddaany927@gmail.com>
2025-10-02 01:15:45 +0900

    Add TopBar Composable

--- feature/main/src/main/java/com/chaeny/moviechart/MainScreen.kt
+++ feature/main/src/main/java/com/chaeny/moviechart/MainScreen.kt
@@ -1,18 +1,51 @@
 package com.chaeny.moviechart
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun MainScreen() {
     Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
+        modifier = Modifier.fillMaxSize()
     ) {
-        Text(text = "Main Screen")
+        TopBar()
+    }
+}
+
+@Composable
+private fun TopBar() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .statusBarsPadding()
+            .padding(start = 25.dp, end = 10.dp, top = 8.dp, bottom = 8.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = stringResource(R.string.movie),
+            fontSize = 20.sp,
+            modifier = Modifier.weight(1f)
+        )
+        IconButton(onClick = {}) {
+            Icon(
+                imageVector = Icons.Default.Search,
+                contentDescription = stringResource(R.string.search)
+            )
+        }
     }
 }
