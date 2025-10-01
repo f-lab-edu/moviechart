1ab05040fd5dba15e4f93695880897a6c1d33043
parkcheayeon <ddaany927@gmail.com>
2025-10-02 00:47:09 +0900

    Add MainScreen Composable

--- app/src/main/java/com/chaeny/moviechart/MainActivity.kt
+++ app/src/main/java/com/chaeny/moviechart/MainActivity.kt
@@ -3,13 +3,14 @@ package com.chaeny.moviechart
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.Text
+import androidx.activity.enableEdgeToEdge
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
-            Text("Hello")
+            MainScreen()
         }
     }
 }
