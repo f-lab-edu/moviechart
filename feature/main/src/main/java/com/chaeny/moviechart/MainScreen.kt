df65d80b02f07e8aeba4cbab95298f22f1da77b3
parkcheayeon <ddaany927@gmail.com>
2025-10-03 16:23:07 +0900

    Remove ripple effect from tab selection

--- feature/main/src/main/java/com/chaeny/moviechart/MainScreen.kt
+++ feature/main/src/main/java/com/chaeny/moviechart/MainScreen.kt
@@ -2,6 +2,7 @@ package com.chaeny.moviechart
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -20,6 +21,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -94,7 +96,11 @@ private fun TabItem(
     modifier: Modifier = Modifier
 ) {
     Column(
-        modifier = modifier.clickable(onClick = onClick),
+        modifier = modifier.clickable(
+            onClick = onClick,
+            indication = null,
+            interactionSource = remember { MutableInteractionSource() }
+        ),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(
