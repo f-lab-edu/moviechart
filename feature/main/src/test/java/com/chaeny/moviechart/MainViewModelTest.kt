145811c85ce7e95d12d51144316c3bb25703dccd
parkcheayeon <ddaany927@gmail.com>
2025-10-05 21:43:37 +0900

    test: Add isLoading state test, verify repository calls

--- feature/main/src/test/java/com/chaeny/moviechart/MainViewModelTest.kt
+++ feature/main/src/test/java/com/chaeny/moviechart/MainViewModelTest.kt
@@ -1,10 +1,12 @@
 package com.chaeny.moviechart
 
+import com.chaeny.moviechart.dto.KobisMovie
 import com.chaeny.moviechart.mapper.MovieIdMapper
 import com.chaeny.moviechart.repository.KobisRepository
 import com.chaeny.moviechart.repository.TmdbRepository
 import com.chaeny.moviechart.util.MainCoroutineScopeRule
 import io.mockk.coEvery
+import io.mockk.coVerify
 import io.mockk.mockk
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.junit.Assert.assertEquals
@@ -36,6 +38,37 @@ class MainViewModelTest {
         val viewModel = MainViewModel(kobisRepository, tmdbRepository, movieIdMapper)
 
         assertEquals(TabType.DAILY, viewModel.selectedTab.value)
+    }
+
+    @Test
+    fun `when data loading completes then isLoading should be false`() {
+        coEvery { kobisRepository.getMovies(any()) } returns TEST_KOBIS_MOVIES
+        coEvery { tmdbRepository.getPosterUrl(any()) } returns ""
+
+        val viewModel = MainViewModel(kobisRepository, tmdbRepository, movieIdMapper)
+
+        coVerify { kobisRepository.getMovies(any()) }
+        coVerify { tmdbRepository.getPosterUrl(any()) }
+        assertEquals(false, viewModel.isLoading.value)
+    }
+
+    companion object {
+        private val TEST_KOBIS_MOVIES = listOf(
+            KobisMovie(
+                rank = "1",
+                id = "20243561",
+                name = "어쩔수가없다",
+                salesShareRate = "45.3",
+                accumulatedAudience = "833401"
+            ),
+            KobisMovie(
+                rank = "2",
+                id = "20256757",
+                name = "극장판 체인소 맨: 레제편",
+                salesShareRate = "24.2",
+                accumulatedAudience = "368903"
+            )
+        )
     }
 
 }
